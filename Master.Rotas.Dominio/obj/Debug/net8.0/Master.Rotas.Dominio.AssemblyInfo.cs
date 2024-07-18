543ab766099521f48d51f1e58b79f247928215e5
Aline Maia <TR042045_ADMAZ@MinhaTI.com.br>
2024-07-17 22:18:59 -0300

    feat: add layers

--- Master.Rotas.Dominio/obj/Debug/net8.0/Master.Rotas.Dominio.AssemblyInfo.cs
+++ Master.Rotas.Dominio/obj/Debug/net8.0/Master.Rotas.Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Master.Rotas.Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e6fea425848eaa5c4a57ec95d12e0faecefeb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43fa8af4bd74769a01fa3b58fbbefcf95edade4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Master.Rotas.Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Master.Rotas.Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
